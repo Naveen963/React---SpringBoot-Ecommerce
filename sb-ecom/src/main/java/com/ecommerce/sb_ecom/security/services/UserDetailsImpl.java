85e66a228050353d79ff0ffe4c72a11da2e308d1
Naveen Kumar Chitturi <naveenkumar200101@gmail.com>
2025-06-11 00:32:37 +0530

    User Details and UserDetailsServiceImpl added

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/services/UserDetailsImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/services/UserDetailsImpl.java
@@ -1,13 +1,17 @@
 package com.ecommerce.sb_ecom.security.services;
 
+import com.ecommerce.sb_ecom.model.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @Data
@@ -32,6 +36,14 @@ public class UserDetailsImpl implements UserDetails {
         this.authorities = authorities;
     }
 
+
+    public static UserDetailsImpl build(User user){
+        List<GrantedAuthority>authorities = user.getRoles().stream().
+                map(role->new SimpleGrantedAuthority(role.getRoleName()
+                        .name())).collect(Collectors.toList());
+        return new UserDetailsImpl(user.getUserId(), user.getUserName(), user.getEmail(), user.getPassword(), authorities);
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
@@ -54,16 +66,24 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDetailsImpl that = (UserDetailsImpl) o;
+        return Objects.equals(id, that.id) && Objects.equals(username, that.username) && Objects.equals(email, that.email) && Objects.equals(password, that.password) && Objects.equals(authorities, that.authorities);
+    }
+
 }
