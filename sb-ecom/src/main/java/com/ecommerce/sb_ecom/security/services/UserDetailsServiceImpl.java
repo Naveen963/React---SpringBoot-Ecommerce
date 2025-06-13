e8c0d418e46e1b1c9d69c7de198586fa3935c388
Naveen Kumar Chitturi <naveenkumar200101@gmail.com>
2025-06-13 20:34:33 +0530

    removed empty spaces

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/services/UserDetailsServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/services/UserDetailsServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-
 public class UserDetailsServiceImpl  implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
